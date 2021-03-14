bb89b061b604f9ca231457a95180cd3453a0cb84
Munna Hossain <mayinuddin.munna@gmail.com>
2021-03-14 22:53:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import firebase from "firebase/app";
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from "./firebase.auth"
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState({
+        isSignIn: false,
+        name: '',
+        photo: '',
+        email: ''
+    })
+
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const handleSignIn = () => {
+        firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+    }
+
+
+    return (
+        <div className="App">
+            {/* {
+                user.isSignIn ? <Button onClick={handleSignOut} variant="outline-light">Sign out</Button> :
+                    <Button onClick={handleSignIn} variant="primary">Sign in</Button>
+            } */}
+            <Button onClick={handleSignIn} variant="primary">Sign in</Button>
+            {
+                user.isSignIn && <div>
+                    <p>Welcome {user.name}</p>
+                    <p>Your email : {user.email}</p>
+                    <img src={user.photo} alt="" />
+                </div>
+            }
+        </div>
+    );
 }
 
 export default App;
+
+// https://web.programming-hero.com/web-3/video/web-3-41-8-not-google-user-login-signout-user
---
+++ src/firebase.auth.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAiTmkRgq7R7Uh0_tJFavLWGzs_gslMfk8",
+    authDomain: "ema-john-simple-6c447.firebaseapp.com",
+    projectId: "ema-john-simple-6c447",
+    storageBucket: "ema-john-simple-6c447.appspot.com",
+    messagingSenderId: "872469349375",
+    appId: "1:872469349375:web:185e30c0dd8820493a5dca"
+  };
+export default firebaseConfig;
